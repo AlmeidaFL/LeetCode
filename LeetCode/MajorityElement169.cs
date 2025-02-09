2d5db5c31efc5f8341e61f085ae3821d2afbf2a0
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-02-09 18:38:39 -0300

    Solution memory complexity (O1)

--- LeetCode/MajorityElement169.cs
+++ LeetCode/MajorityElement169.cs
@@ -21,4 +21,28 @@ public class MajorityElement169
         KeyValuePair<int,int>? x = frequencyByValue.FirstOrDefault(x => x.Value > minFrequency);
         return x?.Key ?? -1;
     }
+
+    public int BoyerMoore(int[] nums)
+    {
+        var candidate = -1;
+        var frequency = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (frequency == 0)
+            {
+                candidate = nums[i];
+                frequency++;
+            }
+            else if (candidate != nums[i])
+            {
+                frequency--;
+                continue;
+            }
+
+            frequency++;
+        }
+        
+        return candidate;
+    }
 }
