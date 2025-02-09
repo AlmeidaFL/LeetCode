438ffa0bcdb48d9d132b9350ae8c27a1e84eabbc
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-02-09 18:24:29 -0300

    Majority Element 169

---
+++ LeetCode/MajorityElement169.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode;
+
+public class MajorityElement169
+{
+    public int MajorityElement(int[] nums)
+    {
+        var frequencyByValue = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            var found = frequencyByValue.TryGetValue(num, out _);
+            if (!found)
+            {
+                frequencyByValue.Add(num, 1);
+                continue;
+            }
+            frequencyByValue[num] += 1;
+        }
+        
+        var minFrequency = nums.Length / 2;
+        KeyValuePair<int,int>? x = frequencyByValue.FirstOrDefault(x => x.Value > minFrequency);
+        return x?.Key ?? -1;
+    }
+}
