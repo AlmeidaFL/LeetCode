16efaf41fbed67da00201dcadc0488d849b7d571
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-02-10 23:33:50 -0300

    First attempt Best time to buy 121

---
+++ LeetCode/BestTimeToBuyAndSellStocks121.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode;
+
+public class BestTimeToBuyAndSellStocks121
+{
+    public static int MaxProfit(int[] prices)
+    {
+        if (prices.Length < 2)
+        {
+            return 0;
+        }
+        
+        var minDay = prices[0];
+        var maxDay = prices[^1];
+        
+        if (minDay >= maxDay && prices.Length == 2)
+        {
+            return 0;
+        }
+        if (maxDay > minDay && prices.Length == 2)
+        {
+            return maxDay - minDay;
+        }
+        
+        var counterAsc = 1;
+        var counterDec = prices.Length - 2;
+        while (counterAsc <= counterDec)
+        {
+            if (prices[counterAsc] < minDay)
+            {
+                minDay = prices[counterAsc];
+            }
+            if (prices[counterDec] > maxDay)
+            {
+                maxDay = prices[counterDec];
+            }
+
+            counterAsc++;
+            counterDec--;
+        }
+
+        if (minDay >= maxDay)
+        {
+            return 0;
+        }
+        
+        return maxDay - minDay;
+    }
+}
