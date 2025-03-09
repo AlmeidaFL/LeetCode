e95c64da3d9ad96d815b9b97471afadfc4bc4f65
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-03-09 17:53:37 -0300

    Rotate Array O(1) space

--- LeetCode/RotateArray189.cs
+++ LeetCode/RotateArray189.cs
@@ -1,4 +1,6 @@
-﻿namespace LeetCode;
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace LeetCode;
 
 public class RotateArray189
 {
@@ -9,16 +11,39 @@ public class RotateArray189
             return;
         }
 
-
+        var tempList = new int[nums.Length];
+        
         var modK = k % nums.Length;
         var indexRotation = modK;
-        var temp = nums[0];
-        for (var i = 0; i < nums.Length; i++)
+        foreach (var transferredNumber in nums)
         {
-            (nums[indexRotation], temp) = (temp, nums[indexRotation]);
-            indexRotation = (indexRotation + modK) % nums.Length;
+            tempList[indexRotation] = transferredNumber;
+            indexRotation = (indexRotation + 1) % nums.Length;
         }
         
+        Array.Copy(tempList, nums, nums.Length);
+    }
+    
+    public static void RotateV2(int[] nums, int k)
+    {
+        if (nums.Length == 1)
+        {
+            return;
+        }
 
+        k %= nums.Length;
+        Reverse(nums, 0, nums.Length-1);
+        Reverse(nums, 0, k-1);
+        Reverse(nums, k, nums.Length-1);
+    }
+
+    private static void Reverse(int[] numbs, int begin, int end)
+    {
+        while (begin < end)
+        {
+            (numbs[begin], numbs[end]) = (numbs[end], numbs[begin]);
+            begin++;
+            end--;
+        }
     }
 }
