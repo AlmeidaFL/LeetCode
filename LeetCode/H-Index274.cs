878ae30eacb8750edc565a09221a2006bdefea6f
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-03-30 18:56:11 -0300

    HIndex v2

--- LeetCode/H-Index274.cs
+++ LeetCode/H-Index274.cs
@@ -4,18 +4,10 @@ public class HIndex274
 {
     public static int HIndex(int[] citations) 
     {
-        var orderedCitations = citations.OrderByDescending(c => c).ToArray();
-        var hIndex = 0;
-        foreach (var item in orderedCitations)
-        {
-            if (item <= hIndex || item == 0)
-            {
-                break;
-            }
-            
-            hIndex++;
-        }
-        
-        return hIndex;
+        return citations
+            .OrderDescending()
+            .Select((citation, index) => new { Citation = citation, Index = index + 1 })
+            .TakeWhile(x => x.Citation >= x.Index)
+            .Count();
     }
 }
