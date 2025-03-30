2067199b0ff555344987a91e5214077229ccaf6a
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-03-30 18:48:28 -0300

    H index

---
+++ LeetCode/H-Index274.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode;
+
+public class HIndex274
+{
+    public static int HIndex(int[] citations) 
+    {
+        var orderedCitations = citations.OrderByDescending(c => c).ToArray();
+        var hIndex = 0;
+        foreach (var item in orderedCitations)
+        {
+            if (item <= hIndex || item == 0)
+            {
+                break;
+            }
+            
+            hIndex++;
+        }
+        
+        return hIndex;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -2,27 +2,4 @@
 
 using LeetCode;
 
-var node1A = new AddTwoNumbers2.ListNode(3);
-var node1B= new AddTwoNumbers2.ListNode(7);
-
-
-node1A.next = node1B;
-
-
-
-var node2A = new AddTwoNumbers2.ListNode(9);
-var node2B= new AddTwoNumbers2.ListNode(2);
-
-node2A.next = node2B;
-
-
-
-var node = AddTwoNumbers2.AddTwoNumbers(node1A,node2A);
-var numbers = new List<int>();
-while(node!=null)
-{
-    numbers.Add(node.val);
-    node = node.next;
-}
-
-Console.WriteLine(string.Join(",",numbers));
+Console.WriteLine(HIndex274.HIndex([1]));
