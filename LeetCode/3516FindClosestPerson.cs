1fc290ab65dea7c9d54c1f05ea69631b5a5dea46
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-09-03 22:00:29 -0300

    Find Closest Person 3516

---
+++ LeetCode/3516FindClosestPerson.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode;
+
+public class FindClosestPerson3516 {
+    public int FindClosest(int x, int y, int z)
+    {
+        var offSetX = Math.Abs(x - z);
+        var offSetY = Math.Abs(y - z);
+
+        return offSetX == offSetY
+            ? 0
+            : offSetX < offSetY
+                ? offSetX
+                : offSetY;
+    }
+}
