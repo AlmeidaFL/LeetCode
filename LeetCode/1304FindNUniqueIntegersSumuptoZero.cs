45958b0d8ed2fa644bf8a921232b18ba6d648d1c
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-09-07 15:28:44 -0300

    1304. Find N Unique Integers Sum up to Zero

---
+++ LeetCode/1304FindNUniqueIntegersSumuptoZero.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode;
+
+/// <summary>
+/// Given an integer n, return any array containing n unique integers such that they add up to 0.
+/// </summary>
+public class FindNUniqueIntegersSumuptoZero1304 {
+    public int[] SumZero(int n) {
+        if (n == 0)
+        {
+            return [0];
+        }
+        
+        var offSet = n % 2 == 0 ? 0 : 1;
+        var array1 = Enumerable.Range(1, ((n - offSet) + 1) / 2).ToArray();
+        var array2 = Enumerable.Range(1, ((n - offSet) + 1) / 2).Select(x => -x).ToArray();
+        return array1.Concat(array2).Concat(offSet == 1 ? [0] : []).ToArray();
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -2,18 +2,4 @@
 
 using LeetCode;
 
-int[][] grid = [[1,7,3],[9,8,2],[4,5,6]];
-Console.WriteLine(new SortMatrixbyDiagonals3446().SortMatrix(grid));
-PrintMatrix(grid);
-
-static void PrintMatrix(int[][] matrix)
-{
-    for (int i = 0; i < matrix.Length; i++) // percorre as linhas
-    {
-        for (int j = 0; j < matrix[i].Length; j++) // percorre as colunas da linha atual
-        {
-            Console.Write(matrix[i][j] + "\t");
-        }
-        Console.WriteLine();
-    }
-}
+Console.WriteLine(string.Join(",",new FindNUniqueIntegersSumuptoZero1304().SumZero(4)));
