9c2c6c09dca9902cc8dd9a3a12400f2978c3a596
AlmeidaFL <ra00303863@pucsp.edu.br>
2026-01-06 16:48:11 -0300

    1161. Maximum Level Sum of a Binary Tree

---
+++ LeetCode/1161MaximumLevelSumofaBinaryTree.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode;
+
+public class MaximumLevelSumofaBinaryTree1161 {
+    public int MaxLevelSum(TreeNode root)
+    {
+        var queue = new Queue<(TreeNode Node, int Level)>();
+        queue.Enqueue((root, 0));
+        
+        var totalSumPerLevel = 0;
+        var totalSum = int.MinValue;
+        var lastLevel = 0;
+        var levelWithHigherSum = 0;
+        
+        while (queue.Count > 0)
+        {
+            var item = queue.Dequeue();
+
+            if (item.Level > lastLevel)
+            {
+                if (totalSumPerLevel > totalSum)
+                {
+                    totalSum = totalSumPerLevel;
+                    levelWithHigherSum = lastLevel;
+                }
+                totalSumPerLevel = 0;
+            }
+            
+            if (item.Node.left != null)
+                queue.Enqueue((item.Node.left, item.Level + 1));
+            if (item.Node.right != null)
+                queue.Enqueue((item.Node.right, item.Level + 1));
+            
+            lastLevel = item.Level;
+            totalSumPerLevel += item.Node.val;
+        }
+        
+        if (totalSumPerLevel > totalSum)
+        {
+            levelWithHigherSum = lastLevel;
+        }
+        
+        
+        return levelWithHigherSum + 1;
+    }
+}
+
+
+ public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+}
---
+++ LeetCode/1351CountNegativeNumbersinaSortedMatrix.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode;
+
+public class CountNegativeNumbersinaSortedMatrix1351 {
+    public int CountNegatives(int[][] grid)
+    {
+        return CountNegativeNumbers(grid[0]);
+    }
+
+    private int CountNegativeNumbers(int[] numbers)
+    {
+        var leftPointer = 0;
+        var rightPointer = numbers.Length - 1;
+
+        while (leftPointer < rightPointer)
+        {
+            var mid = (int)Math.Ceiling((rightPointer + leftPointer) / 2.0);
+
+            if (numbers[leftPointer] >= 0 && numbers[mid] < 0)
+            {
+                break;
+            }
+            
+            if (numbers[mid] >= 0)
+            {
+                leftPointer = mid;
+            }
+            else
+            {
+                rightPointer = mid;
+            }
+        }
+
+        if (leftPointer == rightPointer && rightPointer == 0 && numbers[leftPointer] < 0)
+        {
+            return numbers.Length;
+        }
+        if (leftPointer == rightPointer && rightPointer == 0 && numbers[leftPointer] >= 0)
+        {
+            return numbers.Length - 1;
+        }
+
+        if (leftPointer == rightPointer && rightPointer == numbers.Length - 1 && numbers[leftPointer] < 0)
+        {
+            return 1;
+        }
+        if (leftPointer == rightPointer && rightPointer == numbers.Length - 1 && numbers[leftPointer] >= 0)
+        {
+            return 0;
+        }
+        
+        return rightPointer - leftPointer;
+    }
+}
