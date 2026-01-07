f93481aaf47cfe426dba64a1c8f02ce877006fde
AlmeidaFL <ra00303863@pucsp.edu.br>
2026-01-07 19:15:30 -0300

    1339. Maximum Product of Splitted Binary Tree

---
+++ LeetCode/1339MaximumProductofSplittedBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode;
+
+public class MaximumProductofSplittedBinaryTree1339
+{
+    private const int Billion = 1_000_000_007;
+    
+    public int MaxProduct(TreeNode root) {
+        var totalSum = GetTotalSum(root);
+        long max = 0;
+        GetMaxMultiplication(root, totalSum, ref max);
+        
+        return (int)(max % Billion);
+    }
+
+    public long GetMaxMultiplication(TreeNode? root, long totalSum, ref long max)
+    {
+        if (root == null)
+            return 0;
+        
+        var x = GetMaxMultiplication(root.left, totalSum, ref max);
+        var y = GetMaxMultiplication(root.right, totalSum, ref max);
+        var nodeVal = root.val + x + y;
+        var otherTree = (totalSum - nodeVal);
+        max = Math.Max((otherTree * nodeVal), max);
+        return root.val + y + x;
+    }
+    
+    public long GetTotalSum(TreeNode? root)
+    {
+        if (root == null)
+            return 0;
+        
+        var x = GetTotalSum(root.left);
+        var y = GetTotalSum(root.right);
+
+        return root.val + x + y;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,17 +8,22 @@
 //         right: new TreeNode(val: -8)),
 //     right: new TreeNode(val: 0));
 
-var a = new TreeNode(
-    val: -100,
-    left: new TreeNode(
-        val: -200,
-        left: new TreeNode(val: -20),
-        right: new TreeNode(val: -5)),
-    right: new TreeNode(val: -300, left: new TreeNode(-10)));
+// var a = new TreeNode(
+//     val: -100,
+//     left: new TreeNode(
+//         val: -200,
+//         left: new TreeNode(val: -20),
+//         right: new TreeNode(val: -5)),
+//     right: new TreeNode(val: -300, left: new TreeNode(-10)));
 
 // var a = new TreeNode(
 //     val: 1,
 //     left: new TreeNode(val: 2),
 //     right: new TreeNode(val: 3));
 
-Console.WriteLine(new MaximumLevelSumofaBinaryTree1161().MaxLevelSum(a));
+var a = new TreeNode(
+     val: 1,
+     left: new TreeNode(val: 2, left: new(4), new(5)),
+     right: new TreeNode(val: 3, right: new(6)));
+
+Console.WriteLine(new MaximumProductofSplittedBinaryTree1339().MaxProduct(a));
